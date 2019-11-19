d64284bd05cd2ac186bf41d80d73b25fe50118ad
Gabrielmc35 <gabrielmc35@hotmail.com>
2019-11-18 21:35:24 -0300

    CreateVacinacao Select Aparecendo

--- src/components/vacinacao/CreateVacinacao.js
+++ src/components/vacinacao/CreateVacinacao.js
@@ -7,50 +7,73 @@ import {firestore} from '../../config/fbConfig'
 class createVacinacao extends Component {
     state = {
         nomeUsuario: '',
-        Vacina: ''
+        Vacina: '',
+        teste:''
         
     }
+    
 
     handleChange = (e) => {
+        console.log(e.target.id)
+
         this.setState({
             [e.target.id]: e.target.value
         })
     }
     handleSubmit = (e) => {
         e.preventDefault();
+        
         console.log(this.state);
         this.props.createVacinacao(this.state);
         this.props.history.push('/');
-
+       
         var db;
        db= firestore;
-       db.collection("users").get().then(function(querySnapshot) {
+       var vacinaas;
+       
+       db.collection("vacinas").get().then(function(querySnapshot) {
         querySnapshot.forEach(function(doc) {
             // doc.data() is never undefined for query doc snapshots
             console.log(doc.id, " => ", doc.data());
+            
         });
     });
+    
     }
 
+    
     render() {
         
         return (
+            
             <div className="container">
                 <form className="white" onSubmit={this.handleSubmit}>
-                    <h5 className="grey-text text-darken-3">Criar Nova Vacina</h5>
-                    <div className="input-field">
-                        <input type="text" id='sigla' onChange={this.handleChange} />
-                        <label htmlFor="sigla">Sigla</label>
+                    
+                        
+                    <select class="browser-default">
+      <option value="" disabled selected>Choose your option</option>
+      <option value="1">Option 1</option>
+      <option value="2">Option 2</option>
+      <option value="3">Option 3</option>
+ </select>
+ <div className="input-field">
+                        <input type="text" id='teste' onChange={this.handleChange} />
+                        <label htmlFor="teste">teste</label>
                     </div>
-                   
                     <div className="input-field">
                         <button className="btn pink lighten-1">Create</button>
+                        
+
+
+
                         
                     </div>
 
                 </form>
                 
             </div>
+
+            
         )
     }
 }
