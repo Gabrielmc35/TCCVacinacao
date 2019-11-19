62b9f3efc774604290a58dfc92e0b432ab4b629a
João <joao_marcos_cota@hotmail.com>
2019-11-18 23:18:44 -0300

    Ajustes para carregar combobox.

--- src/components/vacinacao/CreateVacinacao.js
+++ src/components/vacinacao/CreateVacinacao.js
@@ -3,15 +3,16 @@ import { connect } from 'react-redux'
 import { criaVacinacao } from '../../store/actions/vacinaAuctions'
 import { Redirect } from 'react-router-dom'
 import VacinaSummary from '../vacina/VacinaSummary';
-import {firestore} from '../../config/fbConfig'
+import { firestore } from '../../config/fbConfig'
+
 class createVacinacao extends Component {
     state = {
         nomeUsuario: '',
         Vacina: '',
-        teste:''
-        
+        teste: '',
+        dados: undefined,
     }
-    
+
 
     handleChange = (e) => {
         console.log(e.target.id)
@@ -22,58 +23,60 @@ class createVacinacao extends Component {
     }
     handleSubmit = (e) => {
         e.preventDefault();
-        
-        console.log(this.state);
-        this.props.createVacinacao(this.state);
-        this.props.history.push('/');
-       
-        var db;
-       db= firestore;
-       var vacinaas;
-       
-       db.collection("vacinas").get().then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-            
-        });
-    });
-    
     }
+    componentDidMount = async () => {
+        await this.loadData();
+    }
+    loadData = async () => {
+        // console.log(this.state);
+        // this.props.createVacinacao(this.state);
+        // this.props.history.push('/');
 
-    
+        const db = firestore;
+        const querySnapshot = await db.collection("vacinas").get(); // .then((querySnapshot) => {
+        const dados = [];
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            // console.log(doc.id, " => ", doc.data());
+            dados.push({
+                id: doc.id,
+                ...doc.data(),
+            })
+        });
+        this.setState({ dados });
+    };
+
+
     render() {
-        
+        const { dados } = this.state;
         return (
-            
+
             <div className="container">
                 <form className="white" onSubmit={this.handleSubmit}>
-                    
-                        
+
+
                     <select class="browser-default">
-      <option value="" disabled selected>Choose your option</option>
-      <option value="1">Option 1</option>
-      <option value="2">Option 2</option>
-      <option value="3">Option 3</option>
- </select>
- <div className="input-field">
+                        <option value="" disabled selected>Choose your option</option>
+                        {dados && dados.map(x => (
+                            <option key={x.id} value={x.id}>
+                                {x.dose} - {x.estrategia} - {x.idade} - {x.nome} - {x.nomenclaturaAtual} - {x.sigla}
+                            </option>
+                        ))}
+                    </select>
+                    <div className="input-field">
                         <input type="text" id='teste' onChange={this.handleChange} />
                         <label htmlFor="teste">teste</label>
                     </div>
                     <div className="input-field">
                         <button className="btn pink lighten-1">Create</button>
-                        
 
-
-
-                        
                     </div>
 
                 </form>
-                
+
             </div>
 
-            
+
         )
     }
 }
